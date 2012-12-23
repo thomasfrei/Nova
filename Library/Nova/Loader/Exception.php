045bf9b9a07ab863652327c80cb09ef49504fc48
Thomas Frei <thomast.frei@gmail.com>
2012-12-23 12:55:09 +0100

    .

---
+++ Library/Nova/Loader/Exception.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Nova - PHP 5 Framework
+ *
+ * @author      Thomas Frei <thomast.frei@gmail.com>
+ * @copyright   2012 Thomas Frei
+ * @link        https://github.com/thomasfrei/nova
+ * @license 	https://github.com/thomasfrei/nova/blob/master/License.txt 
+ * @package     Nova\Loader
+ * @version     0.0.1 
+ */
+
+namespace Nova\Loader;
+
+/**
+ * Exception handling
+ * 
+ * @package 		Nova\Controller
+ * @subpackage 		Exception	
+ */
+Class Exception extends \Nova\Exception
+{
+		
+}
