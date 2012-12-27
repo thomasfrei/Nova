5132d1a9a83ab2c8384f2c4364dda10ea12bc34b
Thomas Frei <thomast.frei@gmail.com>
2012-12-27 17:19:29 +0100

    Simple Error Controller

---
+++ Application/Modules/Error/Controller/ErrorController.php
@@ -0,0 +1,31 @@
+<?php
+
+class ErrorController extends \Nova\Controller\Action
+{
+    public function errorAction()
+    {
+        $this->view->title = 'Error';
+        $error = $this->_request->getParam('error_handler');
+
+        $this->view->error = $error;
+        $this->view->errorFile = $error->exception->getFile();
+        $this->view->errorLine = $error->exception->getLine();
+        $this->view->errorMsg = $error->exception->getMessage();
+        $this->view->params = $error->request->getParams();
+
+        $counter = 0;
+        $backtrace = array();
+
+        foreach($error->exception->getTrace() as $exception){
+            $backtrace[$counter]['file'] = $exception['file'];
+            $backtrace[$counter]['function'] = $exception['function'];
+            $backtrace[$counter]['line'] = $exception['line'];
+            $counter++;
+        }
+        
+        $this->view->errorTrace = array_reverse($backtrace);
+  
+
+    }
+
+}
