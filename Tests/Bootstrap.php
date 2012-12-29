4bc568a83270a826e6bc32f00698ebb1f904c2c2
Thomas Frei <thomast.frei@gmail.com>
2012-12-29 17:15:02 +0100

    set include paths

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -5,6 +5,13 @@ error_reporting(E_ALL | E_STRICT);
 defined("BASE") or define("BASE", realpath(dirname(__FILE__).'/../') .'/');
 defined("BASEPATH") or define("BASEPATH", BASE . 'Library/');
 defined("APPPATH") or define("APPPATH", BASE . 'Application/');
-defined("TESTPATH") or define("TESTPATH", BASE . 'Tests/testfiles/');
+defined("TESTPATH") or define("TESTPATH", BASE . 'Tests/');
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    BASEPATH,
+    APPPATH,
+    TESTPATH,
+    get_include_path()
+    )));
 
 include BASEPATH."Nova/Exception.php";
